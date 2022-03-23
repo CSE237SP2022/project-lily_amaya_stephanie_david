9cd7deff244155c0bdfca569daf155c67bda0cdf
trevorspiniolas <71231568+trevorspiniolas@users.noreply.github.com>
2022-03-23 14:04:35 -0500

    Created Location Class

---
+++ src/parkSimulator/Location.java
@@ -0,0 +1,13 @@
+package parkSimulator;
+
+public class Location {
+	private String locationName;
+	
+	public Location(String name) {
+		this.locationName = name;
+	}
+	
+	public String getLocationName() {
+		return this.locationName;
+	}
+}
