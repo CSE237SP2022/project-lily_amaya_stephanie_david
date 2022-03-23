d27672129231830a66f0fbb433d2e1eb294d04ef
trevorspiniolas <71231568+trevorspiniolas@users.noreply.github.com>
2022-03-23 13:24:34 -0500

    Initialize Project

---
+++ src/parkSimulator/Simulator.java
@@ -0,0 +1,13 @@
+package parkSimulator;
+
+import java.util.Scanner;
+
+public class Simulator {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello World");
+
+	}
+
+}
