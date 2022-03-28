82b142a203ff27b46f0d7dd4b508478b0c968fc7
awiekerson <77976466+awiekerson@users.noreply.github.com>
2022-03-28 15:25:40 -0500

    Define Garden Object Progress

--- src/parkSimulator/DefineGarden.java
+++ src/parkSimulator/DefineGarden.java
@@ -1,35 +1,43 @@
 package parkSimulator;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class DefineGarden extends Location {
 	
 	private String gardenName;
 	private int numberOfFlowersInHashmap;
-	HashMap<String,Integer> flowersInGarden;
-	Scanner userInput; // create a scanner
+	private Scanner gardenUserInput;
+	private HashMap<String,Integer> flowersInGarden;
 	
 	public DefineGarden(String name) {
 		super(name);
-		gardenName = " ";
+		gardenName = "Botanical Gardens";
+		Scanner gardenUserInput = new Scanner(System.in);
 		numberOfFlowersInHashmap = 0;
 		HashMap<String,Integer> flowersInGarden = new HashMap<String, Integer>();
 		
 	}
 	
-	
 	public void gardenSimulator() {
-		System.out.println("Welcome to the " + gardenName + "! You can plant, pick, and water plants! You can also check the status of your garden using status");
-		//prompt user
-		exitGarden();//pass into exit method
+		System.out.println("Welcome to the " + gardenName + "!");
+		initializeHashmap();
+		prompt();
+		exitGarden();
+	}
+	//option of adding a condition to the flowers where watering and fertilizing them increases the condition
+	
+	public void prompt() {
+		System.out.println("You can plant and pick flowers! You can check the status of your garden typing status and exit back the main menu by typing exit");
+		System.out.println("What would you like to do?");
 	}
 	
-
+	
 	public void initializeHashmap() {
 		flowersInGarden.put("Roses", 4);
 		flowersInGarden.put("Dandalions", 6);
 		flowersInGarden.put("Sunflowers", 10);
 		flowersInGarden.put("Marigolds", 3);
-		flowersInGarden.put("Begonias", 3);
+		flowersInGarden.put("Begonias", 8);
 	}
 	
 	public void printflowerHashMap () {
@@ -37,7 +45,7 @@ public class DefineGarden extends Location {
 	}
 	
 	public void exitGarden () {
-		String inputUser = userInputScanner.nextLine();
+		String inputUser = gardenUserInput.nextLine();
 		if (inputUser.equals("exit")) {
 			returnToParkEntrance();
 		}
