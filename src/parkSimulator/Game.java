dfc1959fb2467aefaf00dae1171e767debe10ebc
dcivantos <dcivantos20@gmail.com>
2022-03-28 18:06:02 -0500

    DefineBasketballCourt finished for iteration 1

---
+++ src/parkSimulator/Game.java
@@ -0,0 +1,32 @@
+package parkSimulator;
+
+public class Game {
+	private String team;
+	private String gameMessage;
+
+	
+	public Game(String name) {
+		team = name;
+		gameMessage = "";
+	}
+	
+	public String simulateGame(String team) {
+		int teamAPoints;
+		int teamBPoints;
+		teamAPoints = (int) (Math.random() * 100);
+		teamBPoints = (int) (Math.random() * 100);
+		if(teamAPoints > teamBPoints) {
+			gameMessage = team + "Wins!" + "Score: " + teamAPoints + "-" + teamBPoints;
+		}
+		else if(teamAPoints < teamBPoints){
+			gameMessage = team + "Loses!" + "Score: " + teamAPoints + "-" + teamBPoints;
+
+		}
+		else {
+			gameMessage = "you tied. Score:" + teamAPoints + "-" + teamBPoints;
+		}
+		 
+		return gameMessage;
+	}
+}
+
