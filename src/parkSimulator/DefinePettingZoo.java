832272fc1d8187f4eef4ad7c37f587199ca6984b
stair <f.stephanie@wustl.edu>
2022-03-29 19:12:37 -0500

    Finished Petting Zoo tests

--- src/parkSimulator/DefinePettingZoo.java
+++ src/parkSimulator/DefinePettingZoo.java
@@ -14,19 +14,19 @@ public class DefinePettingZoo extends Location{
 		message = "Welcome to the petting zoo! What would you like to do?";
 	}
 	
-	void pettingZooSimulator(){
+	public void pettingZooSimulator(){
 		while(!userWantsToLeave) {
 			String userInput = prompt();	
 			sendUserToLocation(userInput);
 		}
 	}
 
-	private String prompt() {
-			System.out.println(message + " (Input \"options\" to see what you can do");
+	public String prompt() {
+			System.out.println(message + " (Input \"options\" to see what you can do)");
 			return userInputScanner.nextLine();
 	}
 	
-	private void sendUserToLocation(String userInput) {
+	public void sendUserToLocation(String userInput) {
 		if(userInput.equals("options")) {
 			listOptions();
 		}
@@ -37,17 +37,17 @@ public class DefinePettingZoo extends Location{
 			petBunny();
 		}
 		else {
-			message = "Invalid input. What would you like to do?";
+			System.out.print("Invalid input. What would you like to do?");
 		}
 	}
 	
-	private void listOptions() {
+	public void listOptions() {
 		System.out.println("Input \"pet bunny\" to pet the bunny."
 				+ "\nInput \"leave petting zoo\" to return to the park entrance.");
 		message = "What would you like to do?";
 	}
 	
-	private void petBunny() {
+	public void petBunny() {
 		System.out.println("You are petting a bunny! The bunny's name is Fluffy. She seems happy :)");
 		message = "What would you like to do next?";
 	}
