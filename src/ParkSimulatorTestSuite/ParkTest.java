4adcf3cb794d9e95666f74a4483688f550f47778
awiekerson <77976466+awiekerson@users.noreply.github.com>
2022-03-29 20:41:33 -0500

    park test cases

--- src/ParkSimulatorTestSuite/ParkTest.java
+++ src/ParkSimulatorTestSuite/ParkTest.java
@@ -1,14 +1,38 @@
 package ParkSimulatorTestSuite;
 
 import static org.junit.jupiter.api.Assertions.*;
+import parkSimulator.Location;
+import parkSimulator.Park;
 
 import org.junit.jupiter.api.Test;
+
+import parkSimulator.Park;
 
 class ParkTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void ParkNameTest() {
+		Park myPark = new Park("Cool Park");
+		String TestParkName = myPark.getParkName();
+		assertTrue(TestParkName.equals("Cool Park"));
 	}
 
+	
+	@Test 
+	void LocationCountZero() {
+		Park myPark = new Park("Cool Park");
+		assertTrue(myPark.getLocationCount()==0);
+	}
+	
+	@Test 
+	void addLocationMultiple() {
+		Park myPark = new Park("Cool Park");
+		Location RockWall = new Location("RockWall");
+		Location Pool = new Location("RockWall");
+		myPark.addLocation(RockWall);
+		myPark.addLocation(Pool);
+		
+		assertTrue(myPark.getLocationCount()==2);
+	}
+	
 }
